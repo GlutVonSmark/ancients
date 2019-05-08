0af2ceec83b917ecb2f932dcf00c685739134b98
Juszczyk Piotr (YWX7RWK) <YWX7RWK@UPS.COM>
2019-05-08 14:47:53 +0100

    Updates

---
+++ precache-manifest.7a9b67b19c6a5f29567563b0b896b9e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edab2133f0ddd2de02551f1983fb628",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "d1d051b871a96e9c4574",
+    "url": "/myapp/static/css/main.831bcbf6.chunk.css"
+  },
+  {
+    "revision": "cae37e09ff8abb16f63c",
+    "url": "/myapp/static/js/2.2ba7af8c.chunk.js"
+  },
+  {
+    "revision": "d1d051b871a96e9c4574",
+    "url": "/myapp/static/js/main.c787edb0.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "22c7df073833fef0e1508a36cf0d0f9f",
+    "url": "/myapp/static/media/DIOGENES.22c7df07.ttf"
+  },
+  {
+    "revision": "fe0e5662e65288c0e880bbd2c5e81b1d",
+    "url": "/myapp/static/media/pythia.fe0e5662.ttf"
+  }
+]);
